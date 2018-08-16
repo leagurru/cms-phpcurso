afcdeeb7889ba1de45707dabd49a3b7b62e82ffb
Leandro Gurruchaga <leagurru@gmail.com>
2018-08-16 20:45:28 -0300

    Clase 93 finalizada

--- search.php
+++ search.php
@@ -14,60 +14,65 @@
 
                 <?php
 
-                    if(isset($_POST['submit'])){
-                        $search = $_POST['search'];
-                        $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%' ";
-                        $search_query = mysqli_query($connection, $query);
-                        if(!$search_query){
-                            die("QUERY FAILED" . mysqli_error($connection));
-                        }
-                        $count = mysqli_num_rows($search_query);
-                        if($count == 0){
-                            echo "<h1>Sin resultados</h1>";
-                        } else {
 
-                        $query = "SELECT * FROM posts";
-                        $select_all_posts_query = mysqli_query($connection, $query);
-                        while($row = mysqli_fetch_assoc($select_all_posts_query)){
-                            $post_title = $row['post_title'];
-                            $post_author = $row['post_author'];
-                            $post_date = $row['post_date'];
-                            $post_image = $row['post_image'];
-                            $post_content = $row['post_content'];
-                            ?>
+        if(isset($_POST['submit'])){
+            $search = $_POST['search'];
+            $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%' ";
+            $search_query = mysqli_query($connection, $query);
 
-                            <h1 class="page-header">
-                                Page Heading
-                                <small>Secondary Text</small>
-                            </h1>
-
-                            <!-- First Blog Post -->
-                            <h2>
-                                <a href="#"><?php echo $post_title ?></a>
-                            </h2>
-                            <p class="lead">
-                                by <a href="index.php"><?php echo $post_author ?></a>
-                            </p>
-                            <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
-                            <hr>
-                            <!--                        <img class="img-responsive" src="http://placehold.it/900x300" alt="">-->
-                            <img class="img-responsive" src="images/<?php echo $post_image?>" alt="">
-                            <hr>
-                            <p><?php echo $post_content ?></p>
-                            <a class="btn btn-primary" href="#">Leer más <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-                            <hr>
+            if(!$search_query){
+                die("QUERY FAILED" . mysqli_error($connection));
+            }
+            $count = mysqli_num_rows($search_query);
+            if($count == 0) {
+                echo "<h1>Sin resultados</h1>";
+            } else {
 
 
+//                $query = "SELECT * FROM posts";
+//                $select_all_posts_query = mysqli_query($connection, $query);
+                while($row = mysqli_fetch_assoc($search_query)){
+                    $post_title = $row['post_title'];
+                    $post_author = $row['post_author'];
+                    $post_date = $row['post_date'];
+                    $post_image = $row['post_image'];
+                    $post_content = $row['post_content'];
+                    ?>
 
-                <?php } ?>
+                    <h1 class="page-header">
+                        Page Heading
+                        <small>Secondary Text</small>
+                    </h1>
 
+                    <!-- First Blog Post -->
+                    <h2>
+                        <a href="#"><?php echo $post_title ?></a>
+                    </h2>
+                    <p class="lead">
+                        by <a href="index.php"><?php echo $post_author ?></a>
+                    </p>
+                    <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
+                    <hr>
+                    <!--                        <img class="img-responsive" src="http://placehold.it/900x300" alt="">-->
+                    <img class="img-responsive" src="images/<?php echo $post_image?>" alt="">
+                    <hr>
+                    <p><?php echo $post_content ?></p>
+                    <a class="btn btn-primary" href="#">Leer más <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                    <hr>
 
 
 
-                        }
+                <?php }
 
-                    }
+
+
+
+            }
+
+        }
+
+                ?>
 
 
             </div>
@@ -79,6 +84,5 @@
         <!-- /.row -->
 
         <hr>
-
 
 <?php include "includes/footer.php"; ?>
