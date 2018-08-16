b4c288d8832a8a30e4c8887bf0043c7a5dd13f66
leagurru <leagurru@gmail.com>
2018-08-16 10:16:24 -0300

    cms clase 86

--- includes/db.php
+++ includes/db.php
@@ -11,11 +11,11 @@ foreach ($db as $key => $value){
 
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-
-if($connection){
-    echo "conectado";
-}else{
-    echo "no conectado";
-}
+//
+//if($connection){
+//    echo "conectado";
+//}else{
+//    echo "no conectado";
+//}
 
 ?>
